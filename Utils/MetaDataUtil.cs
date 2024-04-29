09f5066f4305f8455ce4ac18448cc8bb723e35d3
Mohammad Zohar <mohammam.mz@gmail.com>
2024-04-29 19:18:13 +0300

    add try catch statement

--- Utils/MetaDataUtil.cs
+++ Utils/MetaDataUtil.cs
@@ -7,69 +7,90 @@ namespace Invoice.Utils
     {
         public static async Task<int> GetNumber(DataContext context, int tries = 0)
         {
-            using var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable);
             try
             {
-                await transaction.CreateSavepointAsync("transaction"); // begin transaction
-
-
-                var data = await context.MetaDatas.Where(m => m.Name == "Count").FirstOrDefaultAsync();
-                if (data == null)
+                using var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable);
+                try
                 {
-                    throw new Exception("No initail metadata");
-                }
+                    await transaction.CreateSavepointAsync("transaction"); // begin transaction
 
-                if(data.Value == 100)
-                {
-                    data.Value = 1;
-                }
-                else
-                {
-                    data.Value += 1;
-                }
 
-                await context.SaveChangesAsync();
-                await transaction.CommitAsync(); // commit transaction
-                return data.Value;
+                    var data = await context.MetaDatas.Where(m => m.Name == "Count").FirstOrDefaultAsync();
+                    if (data == null)
+                    {
+                        throw new Exception("No initail metadata");
+                    }
+
+                    if(data.Value == 100)
+                    {
+                        data.Value = 1;
+                    }
+                    else
+                    {
+                        data.Value += 1;
+                    }
+
+                    await context.SaveChangesAsync();
+                    await transaction.CommitAsync(); // commit transaction
+                    return data.Value;
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackToSavepointAsync("transaction"); // rollback
+                    if (tries > 10)
+                    {
+                        throw new Exception("reach max tries");
+                    }
+                    return await GetNumber(context, tries+=1);
+                }
             }
-            catch (Exception)
+            catch (System.InvalidOperationException)
             {
-                await transaction.RollbackToSavepointAsync("transaction"); // rollback
                 if (tries > 10)
-                {
-                    throw new Exception("reach max tries");
-                }
-                return await GetNumber(context, tries+=1);
+                    {
+                        throw new Exception("reach max tries");
+                    }
+                    return await GetNumber(context, tries+=1);
             }
-
         }
 
         public static async Task ResetNumber(DataContext context, int tries = 0)
         {
-            using var transaction = context.Database.BeginTransaction();
             try
             {
-                await transaction.CreateSavepointAsync("transaction"); // begin transaction
-
-                var data = await context.MetaDatas.Where(m => m.Name == "Count").FirstOrDefaultAsync();
-                if (data == null)
+                using var transaction = context.Database.BeginTransaction();
+                try
                 {
-                    throw new Exception("No initail metadata");
+                    await transaction.CreateSavepointAsync("transaction"); // begin transaction
+
+                    var data = await context.MetaDatas.Where(m => m.Name == "Count").FirstOrDefaultAsync();
+                    if (data == null)
+                    {
+                        throw new Exception("No initail metadata");
+                    }
+
+                    data.Value = 0;
+                    await context.SaveChangesAsync();
+
+                    await transaction.CommitAsync(); // commit transaction
                 }
-
-                data.Value = 0;
-                await context.SaveChangesAsync();
-
-                await transaction.CommitAsync(); // commit transaction
+                catch (Exception) 
+                {
+                    await transaction.RollbackToSavepointAsync("transaction"); // rollback
+                    if (tries > 10)
+                    {
+                        throw new Exception("reach max tries");
+                    }
+                    await ResetNumber(context, tries+=1);
+                }
             }
-            catch (Exception) 
+            catch (InvalidOperationException)
             {
-                await transaction.RollbackToSavepointAsync("transaction"); // rollback
                 if (tries > 10)
-                {
-                    throw new Exception("reach max tries");
-                }
-                await ResetNumber(context, tries+=1);
+                    {
+                        throw new Exception("reach max tries");
+                    }
+                    await ResetNumber(context, tries+=1);
             }
         }
     }
